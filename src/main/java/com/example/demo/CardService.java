2e050f85f61013528aba13ff339d077b531f2950
grilld9 <m.maksimenkov@g.nsu.ru>
2023-05-26 12:11:31 +0700

    Изменил порт на 8081, пофиксил генерацию карт

--- src/main/java/com/example/demo/CardService.java
+++ src/main/java/com/example/demo/CardService.java
@@ -12,16 +12,25 @@ public class CardService {
     private final CardRepository cardRepository;
 
     public String generateCardCode() {
-        String cardCodeWithoutChecksum = Long.toString((long) (Math.random() * 1000000000000L));
-        byte[] digits = cardCodeWithoutChecksum.getBytes();
-        int checksum = 10 - (((digits[1] + digits[3] + digits[5] + digits[7] + digits[9] + digits[11])
-                * 3 + (digits[0] + digits[2] + digits[4] + digits[6] + digits[8] + digits[10])) % 10);
-        return cardCodeWithoutChecksum.concat(Integer.toString(checksum));
+        long cardCodeWithoutChecksum = (long) (Math.random() * 1000000000000L);
+        long checksum = countChecksum(cardCodeWithoutChecksum);
+        return Long.toString(cardCodeWithoutChecksum).concat(Long.toString(checksum));
+    }
+
+    private long countChecksum(long cardCodeWithoutChecksum) {
+        long[] digits = new long[12];
+        long cardCode = cardCodeWithoutChecksum;
+        for (int i = 11; i >= 0; i--) {
+            digits[i] = cardCode % 10;
+            cardCode /= 10;
+        }
+        return (10 - ((((digits[1] + digits[3] + digits[5] + digits[7] + digits[9] + digits[11]) * 3)
+                + (digits[0] + digits[2] + digits[4] + digits[6] + digits[8] + digits[10])) % 10)) % 10;
     }
 
     public CardResponseDTO getNewCard() {
         String cardCode = generateCardCode();
-        while (!cardRepository.existsByCardCode(cardCode)) {
+        while (cardRepository.existsByCardCode(cardCode)) {
             cardCode = generateCardCode();
         }
         cardRepository.save(Card.builder()
